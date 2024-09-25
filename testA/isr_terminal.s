9344cdad5753363f058fcaf7637cd81790463984
NORT0X <aleksa.ristic001@gmail.com>
2024-09-25 10:48:57 +0200

    Tests

--- testA/isr_terminal.s
+++
@@ -1,9 +0,0 @@
-# file: isr_terminal.s
-
-.section isr
-# prekidna rutina za terminal
-.global isr_terminal
-isr_terminal:
-    ret
-  
-.end
--- testA/isr_timer.s
+++
@@ -1,9 +0,0 @@
-# file: isr_timer.s
-
-.section isr
-# prekidna rutina za tajmer
-.global isr_timer
-isr_timer:
-    ret
-
-.end
